24e4dcd50b6f0e66cc5ca80d551016b9a82bc1ff
fifty <mohamedelameen50>
2021-10-14 10:34:58 +0400

    unit test signInWithCredentials

--- test/auth-view-model-test.dart
+++ test/auth-view-model-test.dart
@@ -15,7 +15,11 @@ String unValidPassword = 'test@123';
 @GenerateMocks([FacebookLogin])
 @GenerateMocks([GoogleSignIn, GoogleSignInAccount, GoogleSignInAuthentication])
 @GenerateMocks([FirebaseAuth, UserCredential, User])
+
+
 main() async {
+
+
   late MockFirebaseAuth _firebaseAuthMock;
   late MockGoogleSignIn _googleSignIn;
   late MockFacebookLogin _facebookLogin;
@@ -25,122 +29,9 @@ main() async {
     _firebaseAuthMock = MockFirebaseAuth();
     _googleSignIn = MockGoogleSignIn();
     _facebookLogin = MockFacebookLogin();
-    authViewModel = AuthViewModel.instance(
-        _firebaseAuthMock, _googleSignIn, _facebookLogin);
+    authViewModel = AuthViewModel.instance(_firebaseAuthMock, _googleSignIn, _facebookLogin);
   });
 
-  group('Get Facebook Credentials', () {
-    _getFakeFacebookResult(FacebookLoginStatus status) {
-      DateTime expires = DateTime.now().add(Duration(days: 1));
-      return FacebookLoginResult(
-        status,
-        FacebookAccessToken.fromMap(
-          {
-            "token": "",
-            "userId": "",
-            "expires": expires.millisecondsSinceEpoch,
-            'permissions': [],
-            'declinedPermissions': [],
-          },
-        ),
-      );
-    }
-
-    _fakeFacebookLogin(FacebookLoginStatus status) {
-      when(_facebookLogin.logIn(permissions: [FacebookPermission.email]))
-          .thenAnswer((_) async {
-        return _getFakeFacebookResult(status);
-      });
-    }
-
-    test(
-      'when sign in with facebook account success then credential is not null',
-      () async {
-        //ARRANGE
-        _fakeFacebookLogin(FacebookLoginStatus.success);
-        //ACT
-        final credentials = await authViewModel.getFacebookCredentials();
-        //ASSERT
-        expect(credentials, isNotNull);
-      },
-    );
-
-    test(
-      'when sign in with facebook account failed then exception thrown',
-      () async {
-        //ARRANGE
-        _fakeFacebookLogin(FacebookLoginStatus.error);
-        //ASSERT
-        expect(authViewModel.getFacebookCredentials(), throwsException);
-      },
-    );
-
-    test(
-      'when sign in with facebook account canceled then exception thrown',
-      () async {
-        //ARRANGE
-        _fakeFacebookLogin(FacebookLoginStatus.cancel);
-        //ASSERT
-        expect(authViewModel.getFacebookCredentials(), throwsException);
-      },
-    );
-  });
-
-  group('Get Google Credentials', () {
-    _getMockGoogleSignInAuthentication() {
-      final googleAuthentication = MockGoogleSignInAuthentication();
-      when(googleAuthentication.accessToken).thenReturn("");
-      when(googleAuthentication.idToken).thenReturn("");
-      return googleAuthentication;
-    }
-
-    _getFakeGoogleAccount() {
-      final googleAccount = MockGoogleSignInAccount();
-      when(googleAccount.authentication).thenAnswer((_) async {
-        return _getMockGoogleSignInAuthentication();
-      });
-      return googleAccount;
-    }
-
-    _fakeGoogleLogin([String? error]) {
-      when(_googleSignIn.signIn()).thenAnswer((_) async {
-        if (error == null)
-          return _getFakeGoogleAccount();
-        else
-          throw Exception(error);
-      });
-    }
-
-    test(
-      'when sign in with google account success then credential is not null',
-      () async {
-        //Assign
-        _fakeGoogleLogin();
-        //ACT
-        final credentials = await authViewModel.getGoogleCredentials();
-        //ASSERT
-        expect(credentials, isNotNull);
-      },
-    );
-
-    test(
-      'when sign in with google account finish with error then exception thrown',
-      () async {
-        //CANCELED
-        _fakeGoogleLogin(GoogleSignIn.kSignInCanceledError);
-        expect(authViewModel.getGoogleCredentials(), throwsException);
-        //NETWORK ERROR
-        _fakeGoogleLogin(GoogleSignIn.kNetworkError);
-        expect(authViewModel.getGoogleCredentials(), throwsException);
-        //SIGN IN FAILED
-        _fakeGoogleLogin(GoogleSignIn.kSignInFailedError);
-        expect(authViewModel.getGoogleCredentials(), throwsException);
-        //SIGN IN REQUIRED ERROR
-        _fakeGoogleLogin(GoogleSignIn.kSignInRequiredError);
-        expect(authViewModel.getGoogleCredentials(), throwsException);
-      },
-    );
-  });
 
   group('Sign in with email and password', () {
 
@@ -195,25 +86,189 @@ main() async {
   });
 
   group('Sign in with credentials', () {
-    _getGoogleCredentials() async {
-      final signInAccount = await _googleSignIn.signIn();
-      final signInAuthentication = await signInAccount!.authentication;
-      return GoogleAuthProvider.credential(
-        accessToken: signInAuthentication.accessToken,
-        idToken: signInAuthentication.idToken,
+
+    _getFakeFacebookResult(FacebookLoginStatus status) {
+      DateTime expires = DateTime.now().add(Duration(days: 1));
+      return FacebookLoginResult(
+        status,
+        FacebookAccessToken.fromMap(
+          {
+            "token": "",
+            "userId": "",
+            "expires": expires.millisecondsSinceEpoch,
+            'permissions': [],
+            'declinedPermissions': [],
+          },
+        ),
       );
     }
 
+    _fakeFacebookLogin(FacebookLoginStatus status) {
+      when(_facebookLogin.logIn(permissions: [FacebookPermission.email]))
+          .thenAnswer((_) async {
+        return _getFakeFacebookResult(status);
+      });
+    }
+
+    _getMockGoogleSignInAuthentication() {
+      final googleAuthentication = MockGoogleSignInAuthentication();
+      when(googleAuthentication.accessToken).thenReturn("");
+      when(googleAuthentication.idToken).thenReturn("");
+      return googleAuthentication;
+    }
+
+    _getFakeGoogleAccount() {
+      final googleAccount = MockGoogleSignInAccount();
+      when(googleAccount.authentication).thenAnswer((_) async {
+        return _getMockGoogleSignInAuthentication();
+      });
+      return googleAccount;
+    }
+
+    _fakeGoogleLogin([String? error]) {
+      when(_googleSignIn.signIn()).thenAnswer((_) async {
+        if (error == null)
+          return _getFakeGoogleAccount();
+        else
+          throw Exception(error);
+      });
+    }
+
+    group('Get Facebook Credentials', () {
+
+      test(
+        'when sign in with facebook account success then credential is not null',
+            () async {
+          //ARRANGE
+          _fakeFacebookLogin(FacebookLoginStatus.success);
+          //ACT
+          final credentials = await authViewModel.getFacebookCredentials();
+          //ASSERT
+          expect(credentials, isNotNull);
+        },
+      );
+
+      test(
+        'when sign in with facebook account failed then exception thrown',
+            () async {
+          //ARRANGE
+          _fakeFacebookLogin(FacebookLoginStatus.error);
+          //ASSERT
+          expect(authViewModel.getFacebookCredentials(), throwsException);
+        },
+      );
+
+      test(
+        'when sign in with facebook account canceled then exception thrown',
+            () async {
+          //ARRANGE
+          _fakeFacebookLogin(FacebookLoginStatus.cancel);
+          //ASSERT
+          expect(authViewModel.getFacebookCredentials(), throwsException);
+        },
+      );
+    });
+
+    group('Get Google Credentials', () {
+
+      test(
+        'when sign in with google account success then credential is not null',
+            () async {
+          //Assign
+          _fakeGoogleLogin();
+          //ACT
+          final credentials = await authViewModel.getGoogleCredentials();
+          //ASSERT
+          expect(credentials, isNotNull);
+        },
+      );
+
+      test(
+        'when sign in with google account finish with error then exception thrown',
+            () async {
+          //CANCELED
+          _fakeGoogleLogin(GoogleSignIn.kSignInCanceledError);
+          expect(authViewModel.getGoogleCredentials(), throwsException);
+          //NETWORK ERROR
+          _fakeGoogleLogin(GoogleSignIn.kNetworkError);
+          expect(authViewModel.getGoogleCredentials(), throwsException);
+          //SIGN IN FAILED
+          _fakeGoogleLogin(GoogleSignIn.kSignInFailedError);
+          expect(authViewModel.getGoogleCredentials(), throwsException);
+          //SIGN IN REQUIRED ERROR
+          _fakeGoogleLogin(GoogleSignIn.kSignInRequiredError);
+          expect(authViewModel.getGoogleCredentials(), throwsException);
+        },
+      );
+    });
+
+    _mockSignInWithCredentialsSuccess(OAuthCredential credential) {
+      final MockUserCredential userCredential = MockUserCredential();
+      when(userCredential.user).thenReturn(MockUser());
+      when(_firebaseAuthMock.signInWithCredential(credential)).thenAnswer((realInvocation) async => userCredential);
+    }
+
+    _mockSignInWithCredentialsField(OAuthCredential credential) async {
+      when(_firebaseAuthMock.signInWithCredential(credential)).thenAnswer((realInvocation) async => throw FirebaseAuthException(code: ''));
+    }
+
+
     test(
-      'when sign in with google account then user is not null and state is authenticated',
+      'when sign in with credential using google credential success then user is not null and state is authenticated',
       () async {
         //ASSIGN
-        final OAuthCredential credential = await _getGoogleCredentials();
+        _fakeGoogleLogin();
+        final googleCredentials = await authViewModel.getGoogleCredentials();
+        _mockSignInWithCredentialsSuccess(googleCredentials);
         //ACT
-        await authViewModel.signInWithCredentials(credential);
+        await authViewModel.signInWithCredentials(googleCredentials);
         //ASSERT
         expect(authViewModel.user, isNotNull);
         expect(authViewModel.authState, AuthState.Authenticated);
+      },
+    );
+
+    test(
+      'when sign in with credential using google credential fails then user is null and state is notAuthenticated',
+      () async {
+        //ASSIGN
+        _fakeGoogleLogin();
+        final googleCredentials = await authViewModel.getGoogleCredentials();
+        _mockSignInWithCredentialsField(googleCredentials);
+        //ASSERT
+        expect(authViewModel.signInWithCredentials(googleCredentials), throwsException);
+        expect(authViewModel.user, isNull);
+        expect(authViewModel.authState, AuthState.NotAuthenticated);
+      },
+    );
+
+
+    test(
+      'when sign in with credential using facebook credential success then user is not null and state is authenticated',
+      () async {
+        //ASSIGN
+        _fakeFacebookLogin(FacebookLoginStatus.success);
+        final facebookCredentials = await authViewModel.getFacebookCredentials();
+        _mockSignInWithCredentialsSuccess(facebookCredentials);
+        //ACT
+        await authViewModel.signInWithCredentials(facebookCredentials);
+        //ASSERT
+        expect(authViewModel.user, isNotNull);
+        expect(authViewModel.authState, AuthState.Authenticated);
+      },
+    );
+
+    test(
+      'when sign in with credential using facebook credential fails then user is null and state is notAuthenticated',
+      () async {
+        //ASSIGN
+        _fakeFacebookLogin(FacebookLoginStatus.success);
+        final facebookCredentials = await authViewModel.getFacebookCredentials();
+        _mockSignInWithCredentialsField(facebookCredentials);
+        //ASSERT
+        expect(authViewModel.signInWithCredentials(facebookCredentials), throwsException);
+        expect(authViewModel.user, isNull);
+        expect(authViewModel.authState, AuthState.NotAuthenticated);
       },
     );
 
@@ -227,5 +282,7 @@ main() async {
         expect(authViewModel.authState, AuthState.NotAuthenticated);
       },
     );
+
   });
+
 }
